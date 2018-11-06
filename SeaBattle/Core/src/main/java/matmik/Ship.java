073544bc48edb642b0eb1cdbccbd740b740bdf78
Alexander <Sonicrider2@yandex.ru>
2018-11-06 17:31:19 +0400

    Кораблик
    Вот он кораблик

---
+++ SeaBattle/Core/src/main/java/matmik/Ship.java
@@ -0,0 +1,39 @@
+package matmik;
+
+public class Ship {
+	private int shipLength;
+	private int hits = 0;
+	private boolean isRotated = false;
+	private boolean isDestroyed = false;
+	
+	
+	public Ship()
+	{
+	}
+
+
+	public int getShipLength() {
+		return shipLength;
+	}
+
+
+	public void setShipLength(int shipLength) {
+		this.shipLength = shipLength;
+	}
+
+
+	public void rotate() {
+		isRotated = !isRotated;
+	}
+	
+	public void hit() {
+		hits++;
+		if (hits >= shipLength)
+			isDestroyed = true;
+	}
+	
+	public boolean destroyed() {
+		return isDestroyed;
+	}
+
+}
